c18f40c7c74473337544369d9589f0cf63f5d7b2
Marcin Niżnik <marcinniznik@MacBook-Pro-Marcin-2.local>
2016-11-11 18:16:20 +0100

    first

---
+++ save/savethekittens/savethekittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savethekittens
+//
+//  Created by Marcin Niżnik on 11.11.2016.
+//  Copyright © 2016 Marcin Niżnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
